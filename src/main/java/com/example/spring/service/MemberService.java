5d0dd7f13933abfc55d18a4caa12f9ad43cddfbf
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2023-10-05 20:52:47 +0900

    service_02

--- src/main/java/com/example/spring/service/MemberService.java
+++ src/main/java/com/example/spring/service/MemberService.java
@@ -4,6 +4,7 @@ import com.example.spring.domain.Member;
 import com.example.spring.repository.MemberRepository;
 import com.example.spring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -13,12 +14,28 @@ public class MemberService {
 
     public Long join(Member member) {
         //같은 이름을 가진 중복 회원X
-        Optional<Member> result = memberRepository.findByName(member.getName());
-        result.ifPresent(m->{
-            throw new IllegalStateException("이미 존재하는 회원 입니다.");
-        });
+        validateDuplicateMember(member);//중복 회원 검증
         memberRepository.save(member);
         return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+
+                });
+    }
+
+    //전체회원 조회
+
+    public List<Member> findMember(){
+        return memberRepository.findAll();
+
+    }
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
 
     }
 }
