f8e5c9120795b78b9a1e595fd6d77bbdef907383
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2023-10-09 19:24:58 +0900

    회원 서비스 test case 마지막

--- src/test/java/com/example/spring/service/MemberServiceTest.java
+++ src/test/java/com/example/spring/service/MemberServiceTest.java
@@ -4,6 +4,7 @@ import com.example.spring.domain.Member;
 import com.example.spring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -13,8 +14,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberServiceTest {
 
 
-    MemberService memberService=new MemberService();
-    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        memberRepository=new MemoryMemberRepository();
+        memberService=new MemberService(memberRepository);
+    }
 
     @AfterEach
     public void afterEach() {
